0690cbb74ad435d026d53d4f594fe4260b7286f1
DR497 <47689875+dr497@users.noreply.github.com>
2024-01-25 13:15:50 +0800

    Add SNS support (#6)
    * add sns devnet support
    
    * add mainnet constants for sns + fix tests

---
+++ src/registry/sns.spec.ts
@@ -0,0 +1,68 @@
+import { describe, expect, it } from 'vitest'
+import { SNS } from './sns'
+
+describe('SNS', () => {
+  const resolver = new SNS()
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined()
+  })
+
+  it.concurrent(
+    'should resolve injective1699472317873.sol',
+    async () => {
+      const result = await resolver.resolve(
+        'injective1699472317873.sol',
+        'testnet'
+      )
+      expect(result).toBe('inj1axwxmevzgvz5esrmzuyjmra3l9vvl7ury7qegq')
+    },
+    10000
+  )
+
+  it.concurrent(
+    'should resolve injective1699472317873',
+    async () => {
+      const result = await resolver.resolve('injective1699472317873', 'testnet')
+      expect(result).toBe('inj1axwxmevzgvz5esrmzuyjmra3l9vvl7ury7qegq')
+    },
+    10000
+  )
+
+  it.concurrent('should return injective1699472317873.sol', async () => {
+    const result = await resolver.lookup(
+      'inj1axwxmevzgvz5esrmzuyjmra3l9vvl7ury7qegq',
+      'testnet'
+    )
+    expect(result).toBe('injective1699472317873.sol')
+  })
+
+  it.concurrent(
+    'should resolve injective1703588265',
+    async () => {
+      const result = await resolver.resolve('injective1703588265', 'mainnet')
+      expect(result).toBe('inj1qeqxtntyndqg336d7uw4pp9dg3sf8yhwadzez2')
+    },
+    10000
+  )
+
+  it.concurrent(
+    'should resolve injective1703588265.sol',
+    async () => {
+      const result = await resolver.resolve(
+        'injective1703588265.sol',
+        'mainnet'
+      )
+      expect(result).toBe('inj1qeqxtntyndqg336d7uw4pp9dg3sf8yhwadzez2')
+    },
+    10000
+  )
+
+  it.concurrent('should return injective.sol', async () => {
+    const result = await resolver.lookup(
+      'inj1qeqxtntyndqg336d7uw4pp9dg3sf8yhwadzez2',
+      'mainnet'
+    )
+    expect(result).toBe('injective1703588265.sol')
+  })
+})
---
+++ src/registry/sns.ts
@@ -0,0 +1,78 @@
+import {
+  MatchaError,
+  MatchaErrorType,
+  NameService,
+  Network
+} from './name-service'
+import { decode, fromWords } from 'bech32'
+
+export const serviceID = 'sns'
+
+const rpcUrls = {
+  mainnet: 'https://injective-rpc.publicnode.com:443',
+  testnet: 'https://testnet.sentry.tm.injective.network:443'
+}
+
+const trimTld = (x: string): string => {
+  if (x.endsWith('.sol')) {
+    return x.slice(0, -4)
+  }
+  return x
+}
+
+export class SNS extends NameService {
+  serviceID = serviceID
+  chain = 'injective'
+  contractAddress = {
+    mainnet: 'inj1v7chmgm7vmuwldjt80utmw9c95jkrch979ps8z',
+    testnet: 'inj1q79ujqyh72p43mhr2ldaly3x6d50rzp3354at3'
+  }
+
+  async resolve(name: string, network: Network): Promise<string> {
+    const client = await this.getCosmWasmClient(rpcUrls[network])
+    try {
+      const result = await client.queryContractSmart(
+        this.contractAddress[network],
+        {
+          resolve: {
+            domain_name: trimTld(name)
+          }
+        }
+      )
+      if (!result) {
+        throw new MatchaError('', MatchaErrorType.NOT_FOUND)
+      }
+      return result
+    } catch (err) {
+      throw new MatchaError('', MatchaErrorType.NOT_FOUND)
+    }
+  }
+
+  async lookup(address: string, network: Network): Promise<string> {
+    const client = await this.getCosmWasmClient(rpcUrls[network])
+    try {
+      const decoded = decode(address)
+      const bytes = fromWords(decoded.words)
+      const addressBytes = [...new Array(12).fill(0), ...bytes]
+
+      const result = await client.queryContractSmart(
+        this.contractAddress[network],
+        {
+          get_domains_for_owner: {
+            owner_chain: 19,
+            owner_address: addressBytes,
+            max_len: 1,
+            domain_offset: undefined
+          }
+        }
+      )
+
+      if (!result) {
+        throw new MatchaError('', MatchaErrorType.NOT_FOUND)
+      }
+      return result.domains.pop() + '.sol'
+    } catch (err) {
+      throw new MatchaError('', MatchaErrorType.NOT_FOUND)
+    }
+  }
+}
